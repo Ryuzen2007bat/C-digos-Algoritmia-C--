acc08fa408c382a8c93a11b1d6b8fd64fb56076d
Ryuzen2007bat <danmosche@gmail.com>
2026-09-19 16:41:12 -0500

    Enunciado y resolución

--- Examples_and_codeblocks/PE 26-2_2.cpp
+++ Examples_and_codeblocks/PE 26-2_2.cpp
@@ -1,3 +1,6 @@
+/*Diseñe un programa en C++, que permita leer un número entero positivo en el sistema de base diez. Luego el programa debe indicar
+en qué bases (del 3 al 9) la representación de dicho número es PAR.
+No puede emplear arreglos, ni funciones predefinidas, ni funciones propias.*/    
 #include<iostream>
 using namespace std;
 int main(){

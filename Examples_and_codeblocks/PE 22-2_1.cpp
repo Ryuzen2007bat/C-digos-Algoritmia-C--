11cb87716e4bf37ed546b4c5eb032ad1eb129f26
Ryuzen2007bat <danmosche@gmail.com>
2026-09-19 16:43:57 -0500

    Enunciado y resolución

--- Examples_and_codeblocks/PE 22-2_1.cpp
+++ Examples_and_codeblocks/PE 22-2_1.cpp
@@ -1,3 +1,8 @@
+/*puntos Escriba un programa que lea "n" números enteros de 3 cifras, halle el MCD de dichos números y luego determine
+los factores primos del MCD. Ejemplo:
+Si los números son 288, 108 y 180
+El MCD es 36
+Los factores primos de 36 son 2 3. */
 #include<iostream>
 using namespace std; 
 int main(){
